f77f4864cb4f67836d732f161fc231bf58eee1dd
joaobdf <joaobdf@hotmail.com>
2023-01-18 15:38:43 -0300

    Adiciona servidor para tratamento de email

---
+++ servidor/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express()
+app.use(bodyParser.json());
+const port = 3030
+
+app.post('/api/send-email', (req, res) => {
+  const formData = req.body
+  console.log(formData);
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "d636b022b358a1",
+      pass: "1c8d807becd9a6"
+    }
+  });
+
+  const { from, to, subject, text, html } = req.body;
+  const mailOptions = {
+    from,
+    to,
+    subject,
+    text,
+    html
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ mensagem: 'Ocorreu um erro ao enviar o e-mail' });
+    } else {
+      console.log('E-mail enviado: ' + info.response);
+      res.json({ message: 'E-mail enviado com sucesso' })
+    }
+  });
+});
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
